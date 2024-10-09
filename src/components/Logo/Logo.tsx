06579fd690088a95130e9340852196cda0514a05
jasontoups <me@jasontoups.com>
2024-10-08 19:36:47 -0700

    fixed text shadow for logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 export const Logo = () => {
   return (
     /* eslint-disable @next/next/no-img-element */
-    <p className="font-dillan text-3xl text-shadow-scaled">Toupsi</p>
+    <p className="font-dillan text-3xl">Toupsi</p>
   )
 }
