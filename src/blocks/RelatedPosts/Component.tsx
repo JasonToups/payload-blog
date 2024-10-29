0c64ad2234212be3c3131fecc3523b2a5c54fc74
jasontoups <me@jasontoups.com>
2024-10-28 22:04:47 -0700

    updates styling on related posts

--- src/blocks/RelatedPosts/Component.tsx
+++ src/blocks/RelatedPosts/Component.tsx
@@ -19,11 +19,11 @@ export const RelatedPosts: React.FC<RelatedPostsProps> = (props) => {
     <div className={clsx('container', className)}>
       {introContent && <RichText content={introContent} enableGutter={false} />}
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 items-stretch">
+      <div className="flex flex-col items-stretch gap-4 md:flex-row md:gap-8">
         {docs?.map((doc, index) => {
           if (typeof doc === 'string') return null
 
-          return <Card key={index} doc={doc} relationTo="posts" showCategories />
+          return <Card key={index} doc={doc} className="sm" relationTo="posts" showCategories />
         })}
       </div>
     </div>
